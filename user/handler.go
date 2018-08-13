4da12222b2de989da336ff75a6f81eaf0e4a01eb
at-phuongvu <phuong.vu@asiantech.vn>
2018-08-13 15:15:49 +0700

    Update user

--- user/handler.go
+++ user/handler.go
@@ -59,5 +59,42 @@ func (u *UserHandler) GetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u *UserHandler) UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	var user, updateUser User
+	id := chi.URLParam(r, "id")
+	_, err := strconv.Atoi(id)
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Invalid ID"})
+		return
+	}
 
+	if u.DB.Where("id = ?", id).First(&user).RecordNotFound() {
+		w.WriteHeader(422)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Record not found"})
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		json.NewEncoder(w).Encode(ErrorResponse{Message: err.Error()})
+		w.WriteHeader(400)
+		return
+	}
+
+	if len(body) < 1 {
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "No body"})
+		w.WriteHeader(400)
+		return
+	}
+
+	err = json.Unmarshal(body, &updateUser)
+	if err != nil {
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Invalid json"})
+		w.WriteHeader(400)
+		return
+	}
+	u.DB.Model(&user).Updates(&updateUser)
+
+	json.NewEncoder(w).Encode(user)
+	return
 }
