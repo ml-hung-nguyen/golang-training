6f0774a498f3edfafaf64ecabb1e02fc380d331e
at-phuongvu <phuong.vu@asiantech.vn>
2018-08-13 15:06:11 +0700

    get user

--- user/handler.go
+++ user/handler.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi"
 	"github.com/jinzhu/gorm"
 )
 
@@ -38,7 +40,22 @@ func (u *UserHandler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u *UserHandler) GetHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	id := chi.URLParam(r, "id")
+	_, err := strconv.Atoi(id)
+	if err != nil {
+		w.WriteHeader(400)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Invalid ID"})
+		return
+	}
 
+	if u.DB.Where("id = ?", id).First(&user).RecordNotFound() {
+		w.WriteHeader(422)
+		json.NewEncoder(w).Encode(ErrorResponse{Message: "Record not found"})
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+	return
 }
 
 func (u *UserHandler) UpdateHandler(w http.ResponseWriter, r *http.Request) {
